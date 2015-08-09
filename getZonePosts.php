5b346169a46b20ff04a080d87afe783af50d9b96
Dylan Rose <dylanrose60@gmail.com>
2015-08-09 00:25:37 -0400

    test

--- getZonePosts.php
+++ getZonePosts.php
@@ -12,22 +12,7 @@ class getZonePosts {
 		echo "called run";
 	}
 
-	public function getZonePosts($zoneId) {
-		/*
-		$posts = [];
 
-		$con = DBConnect::get();
-		$stmt = $con->prepare("SELECT user,user_photo,postText FROM zone_posts WHERE zone = :zoneId");
-		$stmt->bindParam(':zoneId',$zoneId);
-		$stmt->execute();
-		while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
-			$posts[] = $result;
-		}
-
-		echo "test";
-		*/
-		echo "testing";
-	}
 
 	
 
