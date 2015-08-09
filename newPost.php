ee36d9dddd65d7dd423fa734b6a3ac00ab6b0e06
Dylan Rose <dylanrose60@gmail.com>
2015-08-08 23:31:56 -0400

    user profile photo being added to database

--- newPost.php
+++ newPost.php
@@ -8,17 +8,19 @@ class newPost {
 		$post = json_decode(file_get_contents("php://input"),true);
 
 		$user = $post[0];
-		$zone = $post[1];
-		$postText = $post[2];
+		$userPhoto = $post[1];
+		$zone = $post[2];
+		$postText = $post[3];
 
-		$this->insert($user,$zone,$postText);
+		$this->insert($user,$userPhoto,$zone,$postText);
 
 	}
 
-	public function insert($user,$zone,$post) {
+	public function insert($user,$userPhoto,$zone,$post) {
 		$con  = DBConnect::get();
-		$stmt = $con->prepare("INSERT INTO zone_posts (user,zone,postText) VALUES (:user,:zone,:postText)");
+		$stmt = $con->prepare("INSERT INTO zone_posts (user,user_photo,zone,postText) VALUES (:user,:user_photo,:zone,:postText)");
 		$stmt->bindParam(':user',$user);
+		$stmt->bindParam(':user_photo',$userPhoto);
 		$stmt->bindParam(':zone',$zone);
 		$stmt->bindParam(':postText',$post);
 		$stmt->execute();
