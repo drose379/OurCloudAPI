41e440234f86c4522a343922af287adae813c95c
Dylan Rose <dylanrose60@gmail.com>
2015-08-11 07:26:06 -0400

    new gitignore

---
+++ getZonePosts.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'connect.php';
+
+class getZonePosts {
+
+	public function run() {
+		$post = json_decode(file_get_contents("php://input"),true);
+
+		$zoneId = $post[0];
+
+		$this->grabPosts($zoneId);
+	}
+
+	public function grabPosts($zoneId) {
+		$posts = array();
+
+		$con = DBConnect::get();
+		$stmt = $con->prepare("SELECT user,user_photo,postText FROM zone_posts WHERE zone = :zoneId");
+		$stmt->bindParam(':zoneId',$zoneId);
+		$stmt->execute();
+		while($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			$posts[] = $result;
+		}
+
+		echo json_encode($posts);
+	}
+
+
+
+
+}
