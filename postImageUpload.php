481d7d19e42f34408af16d672f3ab73caf84dba8
Dylan Rose <dylanrose60@gmail.com>
2015-08-15 09:28:17 -0400

    echoing correct path from image upload script

--- postImageUpload.php
+++ postImageUpload.php
@@ -7,8 +7,8 @@ class postImageUpload {
 		$imageTmpName = $_FILES["photo"]["tmp_name"];
 
 		$imageResource = imagecreatefromjpeg($imageTmpName);
-		$randomName = rand(1,100000000000000).".jpg";
-		$path = "/var/www/OurCloudAPI/PostImages/" . $randomName;
+		$randomName = rand(1,10000000000000000).".jpg";
+		$path = "http://104.236.15.47/OurCloudAPI/PostImages/" . $randomName;
 		$imageFile = imagejpeg($imageResource,$path);
 
 		echo $path;
