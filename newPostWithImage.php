c1d2a4076d5d70de06a90c97c4ff31d8b5954cd7
Dylan Rose <dylanrose60@gmail.com>
2015-08-15 23:38:22 -0400

    refactoring newPostWithImage script

--- newPostWithImage.php
+++ newPostWithImage.php
@@ -5,13 +5,19 @@ class newPostWithImage {
 	public function run() {
 		$post = json_decode(file_get_contents("php://input"),true);
 
-		$user = $post[0];
-		$userPhoto = $this->formatUrl($post[1]);
-		$zone = $post[2];
-		$postText = $post[3];
-		$postImageUrl = $post[4];
+		$userId = $post[0];
+		$user = $post[1];
+		$userPhoto = $this->formatUrl($post[2]);
+		$zone = $post[3];
+		$postText = $post[4];
+		$postImageUrl = $post[5];
 
-		$this->insert($user,$userPhoto,$zone,$postText,$postImageUrl);
+		if ($this->userExists($userId)) {
+			$this->insert($userId,$zone,$postText,$postImageUrl);
+		} else {
+			$this->createUser($userId,$userName,$userPhoto);
+			$this->insert($userId,$zone,$postText,$postImageUrl);
+		}
 
 	}
 
@@ -26,12 +32,33 @@ class newPostWithImage {
 		return implode($urlArray);
 	}
 
-	public function insert($user,$userPhoto,$zone,$post,$postImageUrl) {
+	public function userExists($userId) {
+		$con = DBConnect::get();
+		$stmt = $con->prepare("SELECT user_id FROM users WHERE user_id = :userId");
+		$stmt->bindParam(':userId',$userId);
+		$stmt->execute();
+		$result = $stmt->fetch();
+
+		$exists = $result[0] == null ? false : true;
+
+		return $exists;
+	}
+
+	public function createUser($userId,$userName,$userPhoto) {
+		$con = DBConnect::get();
+		$stmt = $con->prepare("INSERT INTO users (user_id,user_name,user_image) VALUES (:id,:name,:photo)");
+		$stmt->bindParam(':id',$userId);
+		$stmt->bindParam(':name',$userName);
+		$stmt->bindParam(':photo',$userPhoto);
+		$stmt->execute();
+	}
+
+
+	public function insert($userId,$zone,$post,$postImageUrl) {
 		error_log($postImageUrl);
 		$con  = DBConnect::get();
-		$stmt = $con->prepare("INSERT INTO zone_posts (user,user_photo,zone,postText,postImage) VALUES (:user,:user_photo,:zone,:postText,:postImage)");
-		$stmt->bindParam(':user',$user);
-		$stmt->bindParam(':user_photo',$userPhoto);
+		$stmt = $con->prepare("INSERT INTO zone_posts (user_id,zone,postText,postImage) VALUES (:id,:zone,:postText,:postImage)");
+		$stmt->bindParam(':id',$userId)
 		$stmt->bindParam(':zone',$zone);
 		$stmt->bindParam(':postText',$post);
 		$stmt->bindParam(':postImage',$postImageUrl);
