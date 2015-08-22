a035d95cb191f0c6e278eca02059f5f05a3e4c72
Dylan Rose <dylanrose60@gmail.com>
2015-08-22 10:34:24 -0400

    testing

--- zoneIdCheck.php
+++ zoneIdCheck.php
@@ -43,7 +43,7 @@ class zoneIdGrabber {
 			for($i = 0;$i<sizeOf($matchingZones);$i++) {
 
 				$currentZone = $matchingZones[$i];
-				error_log(implode(",",$currentZone));
+				error_log($currentZone[0]);
 
 			}
 
