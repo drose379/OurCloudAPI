206ac653e5c1fdde26dc903f5ced1df96f028a75
Dylan Rose <dylanrose60@gmail.com>
2015-09-09 16:50:47 -0400

    attempting to log size

--- live_users/index.js
+++ live_users/index.js
@@ -32,7 +32,7 @@ io.sockets.on('connection',function(socket) {
 		
 		rooms[socketZone][socketUserId] = JSON.stringify([socketUserId,socketZone,socketUserName,socketUserImage]);
 		
-		console.log(rooms[socketZone].length);
+		console.log(Object.size(rooms[socketZone]));
 
 		//emit updateActiveUsers event with the array of users in the same room as the socket
 		io.sockets.in(socketZone).emit('updateUsers',rooms[socketZone]);
