7bf14d6c2a7fd02ad2a500fd3791cdf6b18dc682
Dylan Rose <dylanrose60@gmail.com>
2015-09-08 22:23:43 -0400

    fixes

--- live_users/index.js
+++ live_users/index.js
@@ -1,14 +1,8 @@
 var http = require('http');
 var io = require('socket.io').listen(3000);
 
-var server = http.createServer()
-
 io.sockets.on('connection',function(socket) {
 	console.log("Connection Made!");
 	socket.join("Test");
 });
 
-
-server.listen(3000,function() {
-	console.log("Listening on 3000");
-});
