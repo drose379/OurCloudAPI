a66a78568a23b58f32b0a852b918033458887ac1
Dylan Rose <dylanrose60@gmail.com>
2015-09-09 19:35:55 -0400

    testing disconnection

--- live_users/index.js
+++ live_users/index.js
@@ -27,8 +27,7 @@ io.sockets.on('connection',function(socket) {
 		}
 		
 		rooms[socketZone][socketUserId] = JSON.stringify([socketUserId,socketZone,socketUserName,socketUserImage]);
-	
-
+		
 		//emit updateActiveUsers event with the array of users in the same room as the socket
 		io.sockets.in(socketZone).emit('updateUsers',rooms[socketZone]);
 
@@ -39,7 +38,9 @@ io.sockets.on('connection',function(socket) {
 		var room = rooms[socketZone];
 		delete room[socketUserId];
 
-		io.sockets.in(socketZone).emit('updateUsers',room[socketZone]);
+		console.log(Object.keys(room));
+
+		//io.sockets.in(socketZone).emit('updateUsers',room[socketZone]);
 	});
 
 });
