616c25f07e9473c0e6ca0ad8598e0f453439f46e
Dylan Rose <dylanrose60@gmail.com>
2015-09-08 21:46:40 -0400

    routing requests

--- live_users/index.js
+++ live_users/index.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.get("/",function(request,response){
+
+});
 
 io.on('connection',function(socket) {
 	console.log("Connection Made!");
