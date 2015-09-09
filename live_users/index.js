d8647a5a07041133044d5efa0044f15fa80b411b
Dylan Rose <dylanrose60@gmail.com>
2015-09-09 06:45:52 -0400

    pulling user info

--- live_users/index.js
+++ live_users/index.js
@@ -1,16 +1,23 @@
 var http = require('http');
 var io = require('socket.io').listen(3000);
 
+var rooms = {}
+
 io.sockets.on('connection',function(socket) {
 
-	console.log("Connection Made!");
+	var socketUserId;
+	var socketZone;
 
 	socket.on('disconnect',function() {
 		console.log("Socket disconnected!");
 	});
 
-	socket.on('roomName',function(data) {
-		console.log(data);
+	socket.on('userInfo',function(data) {
+		//also make sure the socket joins this zone as a "room".
+
+		var userData = JSON.parse(data);
+		console.log(userData[0]);
+
 	})
 
 });
