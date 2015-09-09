f1ba2852509b3593c3d015b5569864e6b0d68077
Dylan Rose <dylanrose60@gmail.com>
2015-09-08 22:25:32 -0400

    testing

--- live_users/index.js
+++ live_users/index.js
@@ -2,7 +2,12 @@ var http = require('http');
 var io = require('socket.io').listen(3000);
 
 io.sockets.on('connection',function(socket) {
+
 	console.log("Connection Made!");
-	socket.join("Test");
+
+	socket.on('disconnect',function() {
+		console.log("Socket disconnected!");
+	});
+
 });
 
