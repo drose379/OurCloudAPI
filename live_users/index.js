c147b00f7bd81baa60642f9212da1e7622cb40ce
Dylan Rose <dylanrose60@gmail.com>
2015-09-08 21:50:57 -0400

    testing routing

--- live_users/index.js
+++ live_users/index.js
@@ -2,8 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get("/",function(request,response){
-
+app.get("/",function(request,response) {
+	response.send("testing");
 });
 
 io.on('connection',function(socket) {
