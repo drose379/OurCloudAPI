19944449c7a58fec53ab61320b466122822067e0
Dylan Rose <dylanrose60@gmail.com>
2015-09-09 20:47:43 -0400

    error fixes

--- live_users/index.js
+++ live_users/index.js
@@ -32,6 +32,8 @@ io.sockets.on('connection',function(socket) {
 	});
 
 	socket.on('disconnect',function() {
+		socket.leave(socketZone);
+
 		var room = rooms[socketZone];
 		delete room[socketUserId];
 
