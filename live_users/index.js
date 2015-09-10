f9d3055264403925af0b5bf5205650061a4e6e41
Dylan Rose <dylanrose60@gmail.com>
2015-09-09 20:51:04 -0400

    logging when socket leaves

--- live_users/index.js
+++ live_users/index.js
@@ -32,6 +32,7 @@ io.sockets.on('connection',function(socket) {
 	});
 
 	socket.on('disconnect',function() {
+		console.log("Socket disconnected");
 		socket.leave(socketZone);
 
 		var room = rooms[socketZone];
