c1f1a9615afff68e8b676844fb28c495ff5e0cf7
Dylan Rose <dylanrose60@gmail.com>
2015-09-10 21:35:12 -0400

    broadcasting to other sockets in room

--- live_users/index.js
+++ live_users/index.js
@@ -20,28 +20,15 @@ io.sockets.on('connection',function(socket) {
 		socketUserImage = userData[3];
 
 		socket.join("UNH-Secure");
-/*
-		if (socketZone in rooms == false) {
-			rooms[socketZone] = {};
-		}
-*/
-		// only emit the current list to the socket joining, then later, update the list and broadcast to all sockets in the room besides the new one
-		// since the socket just joining does not need to know about itself
-		
-		//socket.emit('updateUsers',JSON.stringify(rooms[socketZone]));
-		
 
-		//socket.broadcast.to(socketZone).emit('updateUsers',JSON.stringify(rooms[socketZone]));
-
-		io.sockets.in("UNH-Secure").emit('updateUsers',"Sent on connection");
+		socket.broadcast.to("UNH-Secure").emit('updateUsers',"Sent on connection");
 
 	});
 
 	socket.on('disconnect',function() {
-		socket.leave(socketZone);
+		socket.leave("UNH-Secure");
 		
-		//socket.broadcast.to(socketZone).emit('updateUsers',JSON.stringify(rooms[socketZone]));
-		io.sockets.in("UNH-Secure").emit('updateUsers',"Sent on disconnect");
+		socket.broadcast.to("UNH-Secure").emit('updateUsers',"Sent on disconnect");
 	});
 
 });
