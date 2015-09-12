921fb0856941030a9d575c75078aaef7a6b73b61
Dylan Rose <dylanrose60@gmail.com>
2015-09-11 23:13:09 -0400

    creating zone object

--- live_users/index.js
+++ live_users/index.js
@@ -2,6 +2,7 @@
 var io = require('socket.io').listen(3000);
 	
 var userCount = 0;
+var rooms = {};
 
 io.sockets.on('connection',function(socket) {
 
@@ -9,8 +10,6 @@ io.sockets.on('connection',function(socket) {
 	var userZone;
 	var userName;
 	var userImage;
-
-	console.log("Connection");
 
 	//rooms not working properly. attempt to implement own room functionality with filtering a master object,, TRY NAMESPACES
 
@@ -24,6 +23,11 @@ io.sockets.on('connection',function(socket) {
 		userImage = jsonUserInfo[3];
 
 		socket.join(userZone);
+
+		if (userZone in rooms == false) {
+			rooms[userZone] = {};
+			console.log("Created new room for " + userZone);
+		}
 
 		io.sockets.in(userZone).emit('updateUsers',"User Joined! " + userName);
 
