1ce2529875b3f81f10398f868068017e4df154c3
Dylan Rose <dylanrose60@gmail.com>
2015-09-24 11:24:58 -0400

    pulling correct item from result

--- live_users/processMessage.php
+++ live_users/processMessage.php
@@ -27,7 +27,7 @@ class processMessage {
 		while ($result = $stmt->fetch(PDO::FETCH_ASSOC)) {
 			$gcmId = $result;
 		}
-		error_log(json_encode($gcmId));
+		error_log($gcmId["user_gcm_id"]);
 	}
 
 }
