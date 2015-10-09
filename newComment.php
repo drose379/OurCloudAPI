d4cf02913b20e8a9506f0b9c21b40ffdb9a69eea
Dylan Rose <dylanrose60@gmail.com>
2015-10-09 05:43:54 -0400

    testing join query

--- newComment.php
+++ newComment.php
@@ -43,7 +43,8 @@ class newComment {
 
 		//$stmt = $con->prepare("SELECT user_gcm_id FROM users WHERE user_id = :user_id"); // this is the user id for the person making the comment, need to get the id for the OP of the post (2 queries)
 
-		$stmt = $con->prepare("SELECT user_id FROM zone_posts WHERE ID = :post_id");
+		//$stmt = $con->prepare("SELECT user_id FROM zone_posts WHERE ID = :post_id"); // replace with join statement to get the user_gcm_id from users according to the OPS user_id
+		$stmt = $con->prepare("SELECT user_gcm_id FROM zone_posts JOIN zone_posts.user_id ON users.user_id = zone_posts.user_id WHERE ID = :postId");
 
 		$stmt->bindParam(':post_id',$postId);
 		$stmt->execute();
