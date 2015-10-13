4b62e6b23da06f106aeff8c44448cdaa8de76f5f
Dylan Rose <dylanrose60@gmail.com>
2015-10-13 19:19:16 -0400

    testing new zone id grab script

--- zoneIdCheck2.php
+++ zoneIdCheck2.php
@@ -1,20 +1,47 @@
 <?php
 
+require 'connect.php';
+
 class zoneIdCheck2 {
 
 	private $zoneSSID;
-	private $networksInRange;
 
 	public function run() {
 
 		$post = json_decode( file_get_contents("php://input"), true );
 
 		$this->zoneSSID = $post[0];
-		$this->networksInRange = $post[1];
+		$networksInRange = array_unique($post[1]);
 
-		error_log( json_encode( $this->networksInRange ) );
+		$networksInRange = $this->removeNetworkBlanks( $networksInRange );  //untested
 
-		echo json_encode(["ID" => "577739225", "name" => "UNH"]);
+		$zoneInfo = $this->getZoneInfo( $networksInRange );
+
+		echo $zoneInfo;
 	}
+
+	public function removeNetworkBlanks( $networks ) {
+
+		//remove blank items
+		foreach ( $networks as $network ) {
+			if ($network == null) {
+				unser($networks[$network]);
+			}
+		}
+
+		return $networks;
+	}
+
+	public function getZoneInfo( $networks ) {
+		$con = DBConnect::get();
+		$stmt = $con->prepare("SELECT ID,name FROM zones WHERE SSID = :ssid");
+		$stmt->bindParam(':ssid',$this->zoneSSID);
+		$stmt->execute();
+
+		$row = $stmt->fetch( PDO::FETCH_ASSOC );
+
+		return ["ID" => $row["ID"], "name" => $row["name"]];
+	}
+
 
 }
